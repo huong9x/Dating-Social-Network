42a60f37b84800b273a812301f76c68c3503ec07
captainco <nguyenquocco@gmail.com>
2019-05-07 14:59:08 +0700

    delete file database and app

--- http/router.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router');
-const UserController = require('./user-controller');
-
-const router = new Router();
-
-router.get('/users', UserController.showUsers);
-
-module.exports = router.routes();
