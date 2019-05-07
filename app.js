459a1676df7564bfedfd55ed2b4a230c4c4af2e6
huong9x <huongnv@sphinx-software.com>
2019-05-07 17:07:45 +0700

    update css

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const database       = require('./database/database');
 const config         = require('./knexfile');
 const routes         = require('./http/router');
 const njProvider     = require('./nunjucks.provider');
+const path           = require('path');
+const static         = require('koa-static');
 
 const app = new Koa();
+
+const staticPath = './views';
+
+app.use(static(
+  path.join( __dirname,  staticPath)
+));
+
 
 app.use(njProvider());
 app.use(database.connectionProvider(config));
