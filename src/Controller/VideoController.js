8eec9a23dcf452f0f93205a575d24f140cfc92f7
huong9x <huongnv@sphinx-software.com>
2019-05-13 15:35:48 +0700

    update profile videos view

---
+++ src/Controller/VideoController.js
@@ -0,0 +1,20 @@
+class VideoController {
+    constructor(knex) {
+        this.knex = knex;
+    }
+
+    async goVideos(ctx) {
+        return ctx.redirect('/videos/' + ctx.session.loggedInUserId);
+    }
+
+    async getVideos(ctx) {
+        let main_user = await ctx.userRepository.getUserInfo(ctx.session.loggedInUserId);
+        let user = await ctx.userRepository.getUserInfo(ctx.params.userid);
+        if(!user) {
+            return ctx.render('404Page.html', { main_user });
+        }
+        return await ctx.render('videos.html', { user, main_user });        
+    }
+}
+
+module.exports = VideoController;
