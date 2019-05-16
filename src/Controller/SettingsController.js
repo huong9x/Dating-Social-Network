018292f5da587ec85d11c32c6d956f8107e9054a
captainco <nguyenquocco@gmail.com>
2019-05-16 14:16:27 +0700

    create setting, update information user

--- src/Controller/SettingsController.js
+++ src/Controller/SettingsController.js
@@ -4,16 +4,13 @@ class SettingsController {
     }
 
     async getSettings(ctx) {
-        // if(!ctx.query.id) {
-        //     return ctx.redirect('/settings');
-        // }
         let main_user = await ctx.userRepository.getUserInfo(ctx.session.loggedInUserId);
-        // let user      = await ctx.userRepository.getUserInfo(ctx.query.id);
-        // if(!user) {
-        //     return ctx.render('404Page.html', { main_user });
-        // }
-        // return await ctx.render('settings.html', { ctx, user, main_user });
         return ctx.render('settings.html', { main_user });  
+    }
+    async postEditSettings(ctx) {
+        const {first_name, last_name, email, phone_number, birth_date, gender, address, relationship} = ctx.request.body;
+        await ctx.userRepository.editUser(ctx.session.loggedInUserId, first_name, last_name, email, phone_number, birth_date, gender, address, relationship);
+        return ctx.redirect('/settings');
     }
 }
 
