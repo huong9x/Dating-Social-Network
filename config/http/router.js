9c7b9be8dead094e23109599c3a876c54ff98343
huong9x <huongnv@sphinx-software.com>
2019-05-17 11:51:20 +0700

    update register

--- config/http/router.js
+++ config/http/router.js
@@ -1,4 +1,5 @@
 const Router                    = require('koa-router');
+const multer                    = require('koa-multer');
 const logginRequiredMiddleware  = require('../../Middleware/logginRequiredMiddleware');
 const LoginController           = require('../../src/Controller/LoginController');
 const NewsfeedController        = require('../../src/Controller/NewsfeedController');
@@ -10,6 +11,22 @@ const PhotosController          = require('../../src/Controller/PhotosController
 const VideoController           = require('../../src/Controller/VideoController');
 const SignupController          = require('../../src/Controller/SignupController');
 const PostController            = require('../../src/Controller/PostController');
+
+const storage = multer.diskStorage({
+  //文件保存路径
+  destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+  },
+  //修改文件名称
+  filename: function (req, file, cb) {
+      var fileFormat = (file.originalname).split(".");  //以点分割成数组，数组的最后一项就是后缀名
+      cb(null, Date.now() + "." + fileFormat[fileFormat.length - 1]);
+  }
+})
+
+//加载配置
+const upload = multer({ storage: storage });
+
 
 const router                    = new Router();
 const loginController           = new LoginController();
@@ -40,7 +57,28 @@ router
     .get('/newsfeed', logginRequiredMiddleware, newsfeedController.getNewsfeed)
 
     .get('/post', logginRequiredMiddleware, postController.viewPost)
-    .post('/postStatus', logginRequiredMiddleware, newsfeedController.postStatus)
+    .get('/upload', async (ctx, next) => {
+      ctx.render('index.html', true);
+      await next();
+    })
+    .post('/upload', upload.array('file',2), (ctx, next) => {
+      console.log(ctx.req.files);
+      let a = ctx.req.files.map((file) => file.filename);
+      ctx.body = {
+          filename: a//返回文件名
+      }
+    })
+    .post('/postStatus', logginRequiredMiddleware, upload.array('file', 3), async (ctx, next) => {
+        // const {status} = ctx.request.body;
+        // console.log(status);
+        console.log(ctx.req.files);
+        let a = ctx.req.files.map((file) => file.filename);
+        ctx.body = {
+          filename: a//返回文件名
+      }
+        await next();
+
+    })
     
     .get('/profile', logginRequiredMiddleware, profileController.getProfile)
 
