6690164a61a68f8d00e160de3c9d7b800b43ba69
captainco <nguyenquocco@gmail.com>
2019-05-18 15:34:03 +0700

    update comment

--- Comments/CommentRepository.js
+++ Comments/CommentRepository.js
@@ -6,12 +6,22 @@ class CommentRepository {
         this.knex = knex;
     }
 
+    // async findComment(post_id) {
+    //     // let rawComment = await this.knex.select('*').from('comment').where('post_id', post_id).orderBy('comment_id', 'desc');
+
+    //     let rawComment = await this.knex.select('*').from('comment').where('post_id', post_id);
+    //     if (rawComment.length) {
+    //         return new Comment(rawComment);
+    //     }
+    //     return null;
+    //}
+
     async findComment(post_id) {
-        let rawComment = await this.knex.select('*').from('comment').where('post_id', post_id).orderBy('comment_id', 'desc');
-        if (rawComment.length) {
-            return new Comment(rawComment);
-        }
-        return null;
+        // let rawComment = await this.knex.select('*').from('comment').where('post_id', post_id).orderBy('comment_id', 'desc');
+
+        let comments = await this.knex.select('*').from('comment').where('post_id', post_id).orderBy('comment_id', 'desc');
+        return comments.map((comment) => new Comment(comment.comment_id, comment.user_id, comment.post_id, comment.comment_text, comment.comment_time));
+        
     }
 }
 
--- src/Controller/PostController.js
+++ src/Controller/PostController.js
@@ -28,15 +28,13 @@ class PostController {
         let main_user = await ctx.userRepository.getUserInfo(ctx.session.loggedInUserId);
         let post      = await ctx.postRepository.findPost(ctx.query.id);
         let user      = await ctx.userRepository.getUserInfo(post.getUserId());
-        let comment   = await ctx.commentRepository.findComment(ctx.query.id);
-        let userComment = await ctx.userRepository.getUserInfo(comment.getUserId());
+        let comments  = await ctx.commentRepository.findComment(ctx.query.id);
+        // let userComment = await ctx.userRepository.getUserInfo(comment.getUserId()); userComment
         // let showCountComment = await ctx.commentRepository.showCountComment(ctx.query.id);
-        // console.log(comment);
-        
         if(!ctx.query.id) {
             return ctx.render('404Page.html');
         }
-        return ctx.render('postdetail.html', { post, user, userComment, comment, main_user });
+        return ctx.render('postdetail.html', { post, user, comments, main_user });
     }
 }
 
