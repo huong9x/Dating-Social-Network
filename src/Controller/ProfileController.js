4e8d8df11dbb6db14be7e1be4dd8017c592a03d6
huong9x <huongnv@sphinx-software.com>
2019-06-12 17:49:41 +0700

    edit post right in newsfeed and profile

--- src/Controller/ProfileController.js
+++ src/Controller/ProfileController.js
@@ -68,6 +68,18 @@ class ProfileController {
     async searchNearBy(ctx) {
         return ctx.render('searchnearby.html', { ctx });
     }
+    
+    async postStatus(ctx) {
+        const {status} = ctx.req.body;
+
+        let post = await ctx.postRepository.addNewPost(ctx.session.loggedInUserId, status);
+
+        let data = ctx.req.files.map(file => ({ post_id: post.getPostId(), filename: file.filename}));
+
+        let mediaPost = await ctx.mediaRepository.addMedia(data);
+        
+        return ctx.redirect('/newsfeed');
+    }
 
 }
 
